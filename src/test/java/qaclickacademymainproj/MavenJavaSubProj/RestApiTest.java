e2a2eb303f7c23d2c91ce225cca3b4ba02d9212d
krishuQA <krishna-qa@outlook.com>
2022-03-19 00:02:02 +0530

    Commit message

---
+++ src/test/java/qaclickacademymainproj/MavenJavaSubProj/RestApiTest.java
@@ -0,0 +1,18 @@
+package qaclickacademymainproj.MavenJavaSubProj;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
